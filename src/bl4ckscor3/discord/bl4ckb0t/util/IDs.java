50aa85d8453afb601b8e2f5a06b932ca5657c686
bl4ckscor3 <donalddani9612@gmx.de>
2018-05-07 19:15:04 +0200

    fix a channel id

--- src/bl4ckscor3/discord/bl4ckb0t/util/IDs.java
+++ src/bl4ckscor3/discord/bl4ckb0t/util/IDs.java
@@ -10,7 +10,7 @@ public class IDs
 	public static final long RAQBIT = 140370465699921920L;
 
 	/* Channel IDs */
-	public static final long EXTRUDERS = 329007159167680512L;
+	public static final long EXTRUDERS = 429600069151358986L;
 	public static final long BOT_TESTING = 252537749859598338L;
 	public static final long TESTING = 357166022169591810L;
 	public static final long SERVER_STAFF = 329137747887521792L;
