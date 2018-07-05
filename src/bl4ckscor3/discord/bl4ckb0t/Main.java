f4302096c4fd188db984a6d57656859e6fb3edd2
bl4ckscor3 <donalddani9612@gmx.de>
2018-07-05 14:33:47 +0200

    move changelog to seperate file

--- src/bl4ckscor3/discord/bl4ckb0t/Main.java
+++ src/bl4ckscor3/discord/bl4ckb0t/Main.java
@@ -21,66 +21,6 @@ import sx.blah.discord.handle.impl.events.user.PresenceUpdateEvent;
 import sx.blah.discord.handle.obj.ActivityType;
 import sx.blah.discord.handle.obj.StatusType;
 
-/**
- * v2.0		- The bot now works based on modules. Each feature is a seperate .jar file which can get loaded at runtime
- * 			- Removed Upgrading
- * 			- Update Discord4J
- * 			- Remove an unused dependency
- * 			- Internal changes
- *
- * v1.8.4:	- Added support for all characters to hangman, but still, only characters from the English alphabet can be guessed
- * 			- Added way of guessing the complete word (ignores all characters that are not a letter): -hangman guessingThisWord
- *
- * v1.8.3:	- Fixed black jack not working
- * 			- Fixed players with a blackjack not getting tied with the dealer if he has a blackjack
- *
- * v1.8.2:	- Added support for multiple words in hangman
- * 			- Increased rate limit retry delay
- * 			- Fixed black jack round not ending properly when all players have a blackjack
- * 			- Small black jack display changes
- *
- * v1.8.1:	- Added restriction to hangman words so only letters are allowed
- * 			- Hangman messages now show already guessed letters
- * 			- Already guessed letters can no longer be guessed
- *
- * v1.8:	- Added -hangman
- * 			- Added interface to allow waiting for DMs
- * 			- Messages get resent after being rate limited
- * 			- Potential fix for Black Jack soft locking
- *
- * v1.7:	- Added -blackjack (-bj) command
- *			- Removed CSGO update notifier as it depended on Maunz, who no longer has that feature
- *			- Internal changes
- *
- * v1.6:	- Added a ~~5%~~1% chance to add an automatic reaction to Raqbit's messages
- * 			- Added -prick to turn that on and off
- * v1.5:	- Added -osuacc (-oa) to calculate the accuracy with a given amount of 300s/100s/50s and misses
- *
- * v1.4:	- Added notification when the SecurityCraft server is down
- *
- * v1.3.2:  - Added fallback to WolframAlpha query
- *
- * v1.3.1:	- Potentially fixed disappearing playing text
- *
- * v1.3:	- Added logging
- * 			- Backend rewrite
- * 			- Fixed -upgrades being usable in all channels
- * 			- Fixed rare case where upgrades would not count towards the total
- *
- * v1.2.1:	- Updated to Discord4J 2.9
- *
- * v1.2:	- Mavenized to fix the bot not mentioning and notifying after a CS:GO update
- * 			- Removed playing text timer
- * 			- Added program argument for development (-dev)
- * 			- Added error logging
- * 			- Added command to exit the bot
- *
- * v1.1.1:	- Added 'funny' playing text
- *
- * v1.1: 	- Added upgrade counting in #extruders
- *
- * v1.0: 	- Initial release with CSGO update notifications and -calc for WolframAlpha calculations
- */
 public class Main
 {
 	private static boolean dev;
