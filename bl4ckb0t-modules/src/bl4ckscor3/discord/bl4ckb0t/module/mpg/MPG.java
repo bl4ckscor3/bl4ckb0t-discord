94e0d1a7d46b99fa2fed27b983db10feb99e34b7
bl4ckscor3 <donalddani9612@gmx.de>
2018-08-16 20:40:13 +0200

    add mpg module
    original code by akino_germany

---
+++ bl4ckb0t-modules/src/bl4ckscor3/discord/bl4ckb0t/module/mpg/MPG.java
@@ -0,0 +1,49 @@
+package bl4ckscor3.discord.bl4ckb0t.module.mpg;
+
+import bl4ckscor3.discord.bl4ckb0t.AbstractModule;
+import bl4ckscor3.discord.bl4ckb0t.util.Utilities;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+
+/**
+ * @author akino_germany, original code at https://github.com/bl4ckscor3/bl4ckb0t/blob/master/MPG/src/bl4ckscor3/module/mpg/ChannelCommand.java
+ */
+public class MPG extends AbstractModule
+{
+	public MPG(String name)
+	{
+		super(name);
+	}
+
+	@Override
+	public void exe(MessageReceivedEvent event, String[] args) throws Exception
+	{
+		IChannel channel = event.getChannel();
+		String result = "";
+		float calc = 1.0f;
+		float calc2 = 2.0f;
+		if(args.length == 2) {
+			if(args[0].equals("us")) {
+				calc = 235/(Float.parseFloat(args[1]));
+				result = String.format("%s mpg (US) sind %f l/100km", args[1], calc);
+				Utilities.sendMessage(channel, result);
+			} else if(args[0].equals("imp")) {
+				calc = 282/(Float.parseFloat(args[1]));
+				result = String.format("%s mpg (Imperial) sind %f l/100km", args[1], calc);
+				Utilities.sendMessage(channel, result);
+			}
+		} else if(args.length == 1) {
+			calc = 235/(Float.parseFloat(args[0]));
+			calc2 = 282/(Float.parseFloat(args[0]));
+			result = String.format("%s l/100km sind %f mpg (US) und %f mpg (Imperial) ", args[0], calc, calc2);
+			Utilities.sendMessage(channel, result);
+		}
+
+	}
+
+	@Override
+	public boolean triggeredBy(MessageReceivedEvent event)
+	{
+		return event.getMessage().getContent().startsWith("-mpg");
+	}
+}
