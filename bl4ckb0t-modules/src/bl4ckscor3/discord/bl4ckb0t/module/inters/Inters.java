4e8658bdb502dc9d84425159b8f0c5f04ee30113
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-04-27 17:39:06 +0200

    add a funny inter(n)s command

---
+++ bl4ckb0t-modules/src/bl4ckscor3/discord/bl4ckb0t/module/inters/Inters.java
@@ -0,0 +1,26 @@
+package bl4ckscor3.discord.bl4ckb0t.module.inters;
+
+import bl4ckscor3.discord.bl4ckb0t.AbstractModule;
+import bl4ckscor3.discord.bl4ckb0t.util.Utilities;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class Inters extends AbstractModule {
+	public Inters(String name) {
+		super(name);
+	}
+
+	@Override
+	public void exe(MessageReceivedEvent event, String[] args) throws Exception {
+		if (event.getMessage().getContentRaw().equals(".interns"))
+			Utilities.sendMessage(event.getChannel(), "https://tenor.com/view/patrick-star-idk-hammer-nailed-gif-15254720");
+		else
+			Utilities.sendMessage(event.getChannel(), "https://imgur.com/7Y3cFKt");
+	}
+
+	@Override
+	public boolean triggeredBy(MessageReceivedEvent event) {
+		String content = event.getMessage().getContentRaw();
+
+		return content.equals(".interns") || content.equals(".inters");
+	}
+}
