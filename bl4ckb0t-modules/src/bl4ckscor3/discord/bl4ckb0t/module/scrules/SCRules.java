d498ff070e2a13abe1323638d13c7baa03e37a4c
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-08-31 22:41:39 +0200

    Update SCRules with changed rules

--- bl4ckb0t-modules/src/bl4ckscor3/discord/bl4ckb0t/module/scrules/SCRules.java
+++ bl4ckb0t-modules/src/bl4ckscor3/discord/bl4ckb0t/module/scrules/SCRules.java
@@ -18,18 +18,16 @@ import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 public class SCRules extends AbstractModule {
 	private static final long SECURITYCRAFT_GUILD_ID = 318542314010312715L;
 	private static final Map<String, Rule> RULES = new HashMap<>();
-	private static final String RULE_1 = "1. Don't mention staff";
 
 	static {
-		RULES.put(RULE_1, new Rule("Don't mention users that have the `Moderators`, `Developers`, or `Owner` roles without an urgent cause (examples of an urgent cause: our official Minecraft server is down, critical crash in new version, ...). When using Discord's Reply functionality, remember to turn pinging off.", "1️⃣"));
-		RULES.put("2. English only", new Rule("This is an English server. We cannot moderate non-English conversations, so please only talk English. Feel free to use a translation service.", "2️⃣"));
-		RULES.put("3. Wrong channel", new Rule("Keep messages in the correct channels. Check the channel description before writing! If your message does not fit the channel at all, it may be removed without notice.", "3️⃣"));
-		RULES.put("4. Lack of respect", new Rule("Treat others with respect.", "4️⃣"));
-		RULES.put("5. Spam", new Rule("Do not spam in any way, shape, or form. This includes NSFW content.", "5️⃣"));
-		RULES.put("6. Cracked launchers", new Rule("Don't ask for help if you are using cracked launchers like TLauncher or similar. We do not support them, and SecurityCraft may not work. You're on your own.", "6️⃣"));
-		RULES.put("7. Backseat moderating", new Rule("Leave moderating to the moderators. If there is a problem, or someone breaking the rules, you can let the `Discord Staff` know (this is an exception to rule 1).", "7️⃣"));
-		RULES.put("8. Asking about updates", new Rule("Don't ask about updates, particularly release ETAs. They will be published eventually. _It's done when it's done._", "8️⃣"));
-		RULES.put("9. Advertising", new Rule("Do not advertise. This includes posting Discord invite links, unless asked about.", "9️⃣"));
+		RULES.put("1. English only.", new Rule("**English only.** This is an English server. We cannot reliably moderate non-English conversations, so please only talk English. Feel free to use a translation service.", "1️⃣"));
+		RULES.put("2. Be friendly to others.", new Rule("**Be friendly to others.** Insults, attacks, and similar have no place here.", "2️⃣"));
+		RULES.put("3. Keep to the channel topic.", new Rule("**Keep to the channel topic.** Channels have topics explaining what can be discussed in them. Please read it before first writing into a channel.", "3️⃣"));
+		RULES.put("4. Do not spam.", new Rule("**Do not spam.** This includes, but is not limited to, sending the same message in multiple channels and advertising your own Discord server or content.", "4️⃣"));
+		RULES.put("5. No NSFW content.", new Rule("**No NSFW content.**", "5️⃣"));
+		RULES.put("6. Keep pinging/mentioning people to a minimum.", new Rule("**Keep pinging/mentioning people to a minimum.** If someone tells you they don't want to get pinged, please respect that. Feel free to set your own preference in <id:customize>.", "6️⃣"));
+		RULES.put("7. Don't ask for help if you are using cracked launchers.", new Rule("**Don't ask for help if you are using cracked launchers** like TLauncher or similar. We do not support them, and SecurityCraft may not work. You're on your own.", "7️⃣"));
+		RULES.put("8. Leave moderating to the moderators.", new Rule("**Leave moderating to the moderators.** If there is a problem or someone is breaking the rules, you can let the @Discord Staff know. If a moderator feels a topic is not suitable for the server, even if not covered by the rules, they may end the conversation. They have the final say.", "8️⃣"));
 	}
 
 	public SCRules(String name) {
@@ -51,9 +49,6 @@ public class SCRules extends AbstractModule {
 						.addField("Please take a moment to review the rules.", "[Click here to see the rules](https://canary.discord.com/channels/318542314010312715/318544502438756352/667401399025401866)", false)
 						.setTimestamp(event.getTimeCreated());
 				//@formatter:on
-
-				if (option.equals(RULE_1))
-					embed.setImage("https://i.imgur.com/lS7cE1f.png");
 
 				event.replyEmbeds(embed.build()).complete();
 			}
